dcca6ea695f6e53a79b4365f657d5bcab90a90db
Devraj Srivastawa <100594605+Devraj5032@users.noreply.github.com>
2024-01-16 12:54:20 +0530

    final

--- script.js
+++ script.js
@@ -1,47 +1,65 @@
 const showImg = document.getElementById("image_show");
 const message = document.getElementById("result-text");
-// const myCanvas = document.getElementById("myCanvas");
-// const ctx = myCanvas.getContext("2d");
 
-document
-  .getElementById("imageInput")
-  .addEventListener("change", function (event) {
-    const fileInput = event.target;
+document.getElementById("imageInput").addEventListener("change", async function (event) {
+  const fileInput = event.target;
 
-    if (fileInput.files.length > 0) {
-      const selectedFile = fileInput.files[0];
-      const render = new FileReader();
+  if (fileInput.files.length > 0) {
+    const selectedFile = fileInput.files[0];
+    const render = new FileReader();
 
-      render.onload = function (e) {
-        message.innerText = "";
-        const base64Image = e.target.result;
-        showImg.src = "./loading.gif";
-        axios({
+    render.onload = async function (e) {
+      message.innerText = "";
+      const base64Image = e.target.result;
+      showImg.src = "./loading.gif";
+
+      try {
+        const response = await axios({
           method: "POST",
-          url: "https://detect.roboflow.com/binpercecntage/2",
+          url: "https://detect.roboflow.com/binsmaterial/1",
           params: {
-            api_key: "so7oMBbo34YblkbWHgTs",
+            api_key: "06AAsOyyx5C7hrsygRQT",
           },
           data: base64Image,
           headers: {
             "Content-Type": "application/x-www-form-urlencoded",
           },
-        })
-          .then(function (response) {
-            showImg.src = base64Image;
+        });
 
-            const data = response.data;
-            if (data.predictions.length > 0) {
-              message.style.color = "green";
-              message.innerText = `Material = ${data.predictions[0]?.class} and Fill value = ${data.predictions[1]?.class}`;
-            }
-            console.log(data);
-          })
-          .catch(function (error) {
-            console.log(error.message);
-          });
-      };
+        showImg.src = base64Image;
 
-      render.readAsDataURL(selectedFile);
-    }
-  });
+        const data = response.data;
+
+        const predictionClasses = [];
+
+        // Looping through predictions in reverse order
+        for (let i = 0; i <= data.predictions.length - 1; i++) {
+          const prediction = data.predictions[i];
+          predictionClasses.push(prediction.class);
+        }
+
+        let specialElementsArray;
+        let restElementsArray
+
+        if (predictionClasses.length > 0) {
+          // Array with unique elements ["60-90", "below 50", "overfill"]
+           specialElementsArray = Array.from(new Set(predictionClasses.filter(element => ["60-90", "below 50", "overfill"].includes(element))));
+
+          // Array with unique elements other than ["60-90", "below 50", "overfill"]
+           restElementsArray = Array.from(new Set(predictionClasses.filter(element => !["60-90", "below 50", "overfill"].includes(element))));
+        }
+
+        console.log(predictionClasses);
+        if (predictionClasses.length > 0) {
+          message.style.color = "green";
+          message.innerText = `Material = ${restElementsArray <= 0 ? "No Material" : restElementsArray.map(pre => pre)} and Fill value = ${specialElementsArray.map(pre => pre)}`;
+        }
+        console.log(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    render.readAsDataURL(selectedFile);
+  }
+});
